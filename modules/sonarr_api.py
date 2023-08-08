1bdf72d1ef9e350d237776dd6bb1877bd910a64f
Jed Giblin <jegiblin@cisco.com>
2023-08-07 20:47:53 -0400

    shows more done

--- modules/sonarr_api.py
+++ modules/sonarr_api.py
@@ -21,6 +21,14 @@ class SonarrApi:
             params['apiKey'] = self._api_key
         return requests.get(uri, params=params, auth=self.__auth())
 
+    def __delete(self, path, params=None):
+        if params is None:
+            params = dict()
+        uri = f'https://{self._host}{path}'
+        if 'apiKey' not in params:
+            params['apiKey'] = self._api_key
+        return requests.delete(uri, params=params, auth=self.__auth())
+
     def __post(self, path, body):
         uri = f'https://{self._host}{path}'
         res = requests.post(uri, params={"apiKey": self._api_key}, json=body)
@@ -52,8 +60,36 @@ class SonarrApi:
         else:
             return [self.create_tag(tag)]
 
+    def search_existing_shows_by_tag(self, tag):
+        """
+        1. Get the ID for the tag
+        2. Fetch usage of tag
+        3. Fetch series in use by that tag
+        :param tag:
+        :return:
+        """
+        tags = next(filter(lambda x: x.get("label") == tag,
+                           self.__get('/api/tag').json()), None)
+        if not tags:
+            return []
+
+        detail = self.__get(f'/api/v3/tag/detail/{tags.get("id")}').json()
+        if not detail:
+            return []
+        series = [ series for series in self.__get(f'/api/v3/series').json() if series["id"] in detail['seriesIds'] ]
+        print(len(series))
+        return series
+
     def search_for_episodes(self, series_id):
         res = self.__post('/api/command', body={'name': 'SeriesSearch', 'seriesId': series_id})
+        return res.json()
+
+    def delete_show(self, series_id):
+        res = self.__delete(f'/api/v3/series/{series_id}', params={'deleteFiles': True})
+        return res
+
+    def get_show(self, series_id):
+        res = self.__get(f'/api/v3/series/{series_id}')
         return res.json()
 
     def _add_show(self, show, seasons, tags):
--- modules/sonarr_manager.py
+++ modules/sonarr_manager.py
@@ -12,14 +12,15 @@ NEW_SHOW_SEARCH = 11
 SHOW_PICKER = 12
 CONFIRM_SHOW = 13
 
+CLEAN_SHOW_SEARCH = 21
+CHOOSE_SHOW_TO_MANAGE = 22
+MANAGE_SHOW = 23
+
 
 async def entry_point(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_keyboard = [
-        [
-            InlineKeyboardButton("Add Shows", callback_data="add_shows"),
-            InlineKeyboardButton("Delete Shows", callback_data="rm_shows"),
-            InlineKeyboardButton("List Shows", callback_data="ls_shows")
-        ]
+        [InlineKeyboardButton("Add Shows", callback_data="add_shows")],
+        [InlineKeyboardButton("Manage Shows", callback_data="list_user_shows")],
     ]
     reply_markup = InlineKeyboardMarkup(reply_keyboard)
     await update.message.reply_text(
@@ -43,15 +44,17 @@ async def add_shows(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return NEW_SHOW_SEARCH
 
 
-async def search_sonarr(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def search_sonarr_new(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query_str = update.message.text
-    sonarr_config = mydb.get_user(update.message.from_user.id)
-    if not sonarr_config:
+    try:
+        sonarr_config = await get_and_validate_config(update.message.from_user.id)
+    except ValueError:
         await context.bot.send_message(
             chat_id=update.message.from_user.id,
             text="You have not configured a server.Exiting."
         )
         return ConversationHandler.END
+
     context.user_data['sonarr'] = SonarrApi(**sonarr_config)
     context.user_data['show_cache'] = {}
     buttons = []
@@ -102,15 +105,98 @@ async def confirm_show_add(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f"The show couldn't be added: {msg}")
 
+    del context.user_data['sonarr']
     return ConversationHandler.END
 
 
-async def rm_shows(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
+async def list_user_shows(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    await query.message.delete()
+
+    try:
+        sonarr_config = await get_and_validate_config(update.effective_user.id)
+    except ValueError:
+        await context.bot.send_message(
+            chat_id=update.message.from_user.id,
+            text="You have not configured a server.Exiting."
+        )
+        return ConversationHandler.END
+
+    context.user_data['sonarr'] = SonarrApi(**sonarr_config)
+
+    btns = [InlineKeyboardButton(text=s['title'], callback_data=f'manage_{s["id"]}') for s in
+            context.user_data['sonarr'].search_existing_shows_by_tag(f'tg:{update.effective_user.id}')]
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Please choose a show to manage",
+        reply_markup=InlineKeyboardMarkup([[btn] for btn in btns])
+    )
+
+    return CHOOSE_SHOW_TO_MANAGE
 
 
-async def ls_shows(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
+async def manage_show_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    show_id = update.callback_query.data.split('_')[-1]
+    await query.message.delete()
+
+    show = context.user_data['sonarr'].get_show(show_id)
+    await context.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        photo=show.get("images")[0].get("remoteUrl")
+    )
+    await context.bot.send_message(
+        text='What would you like to do?',
+        chat_id=update.effective_chat.id,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text='Delete Show', callback_data=f'delete_{show_id}')],
+                [InlineKeyboardButton(text='Download More Seasons', callback_data=f'man_season_{show_id}')]
+            ]
+        )
+    )
+    return MANAGE_SHOW
+
+
+async def delete_show(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    show_id = update.callback_query.data.split('_')[-1]
+    await query.message.delete()
+    res = context.user_data['sonarr'].delete_show(show_id)
+    if res:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='Show Deleted'
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='Failed to delete show'
+        )
+    del context.user_data['sonarr']
+    return ConversationHandler.END
+
+
+async def manage_show_seaons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    show_id = update.callback_query.data.split('_')[-1]
+    await query.message.delete()
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Apologies. This feature is not yet implemented'
+    )
+    return ConversationHandler.END
+
+
+async def get_and_validate_config(user_id):
+    sonarr_config = mydb.get_user(user_id)
+    if not sonarr_config:
+        raise ValueError
+    return sonarr_config
 
 
 CONVERSATION = ConversationHandler(
@@ -118,11 +204,10 @@ CONVERSATION = ConversationHandler(
     states={
         START: [
             CallbackQueryHandler(add_shows, pattern="^add_shows$"),
-            CallbackQueryHandler(rm_shows, pattern="^rm_shows$"),
-            CallbackQueryHandler(ls_shows, pattern="^ls_shows$"),
+            CallbackQueryHandler(list_user_shows, pattern="^list_user_shows"),
         ],
         NEW_SHOW_SEARCH: [
-            MessageHandler(filters=filters.TEXT, callback=search_sonarr)
+            MessageHandler(filters=filters.TEXT, callback=search_sonarr_new)
         ],
         SHOW_PICKER: [
             CallbackQueryHandler(show_clicked, pattern="^add_show_[0-9]+$")
@@ -130,6 +215,13 @@ CONVERSATION = ConversationHandler(
         CONFIRM_SHOW: [
             CallbackQueryHandler(confirm_show_add, pattern="^confirm_[0-9]+$"),
             CallbackQueryHandler(show_clicked, pattern="^add_show_[0-9]+$")
+        ],
+        CHOOSE_SHOW_TO_MANAGE: [
+            CallbackQueryHandler(manage_show_menu, pattern="^manage_[0-9]+$"),
+        ],
+        MANAGE_SHOW: [
+            CallbackQueryHandler(delete_show, pattern="^delete_[0-9]+$"),
+            CallbackQueryHandler(manage_show_seaons, pattern="^man_season_[0-9]+$")
         ]
     },
     fallbacks=[CommandHandler("shows", entry_point)]
