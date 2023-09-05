e051a1d76923e1db0101f5b0469fb123e116a0d6
Jed Giblin <jegiblin@cisco.com>
2023-09-05 08:55:27 -0400

    created trash module

--- modules/trash_monitor.py
+++
@@ -1,6 +0,0 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-
-
-async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
