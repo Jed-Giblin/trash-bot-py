84747c38651fe19e38ad466a10e21cb982e0f89d
Cole Aten <caten@cisco.com>
2023-10-10 10:29:45 -0400

    enforced quit button on failed search

--- modules/readarr_manager.py
+++ modules/readarr_manager.py
@@ -84,7 +84,8 @@ async def list_search_results(update: Update, context: ContextTypes.DEFAULT_TYPE
         chat_id=update.effective_chat.id
     )
 
-    btns = []
+    btns = [[InlineKeyboardButton(text="Quit! (not a book)", callback_data=f'quit')]]
+    found_books = False
     for book in sorted(context.user_data['readarr'].search_books(search_str),
                        key=(lambda x: x.get('book', {}).get('ratings', {}).get('value', 0)), reverse=True)[
                 0:50]:
@@ -99,10 +100,15 @@ async def list_search_results(update: Update, context: ContextTypes.DEFAULT_TYPE
             slug = f'{slug} ({book["seriesTitle"]})'
         slug = f'{slug} - {author}'
         btns.append([InlineKeyboardButton(text=slug, callback_data=f"detail_{bk_id}")])
+        found_books = True
 
-    btns.insert(0, [InlineKeyboardButton(text="Quit! (not a book)", callback_data=f'quit')])
+    if found_books:
+        text_content = 'Here are the top results. You can also search again by sending me a new message'
+    else:
+        text_content = 'Search returned no results at this time, please try again or use a different search term'
+
     await context.bot.send_message(
-        text='Here are the top results. You can also search again by sending me a new message',
+        text=text_content,
         chat_id=update.effective_chat.id,
         reply_markup=InlineKeyboardMarkup(btns)
     )
