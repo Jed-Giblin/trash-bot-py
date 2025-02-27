c60078d6081655a6f60d43478853e74f51cc8344
Jed Giblin <jegiblin@cisco.com>
2025-02-27 16:26:56 -0500

    adjusted regex

--- modules/trash.py
+++ modules/trash.py
@@ -59,16 +59,12 @@ async def handle_trash(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return None
 
     msg_txt = update.message.text if update.message.text else update.message.caption if update.message.caption else ''
-    print(msg_txt)
     mid_regex = "[\s]?|[\^\s$]".join(context.chat_data.words)
     regex = f'[\^\s]?{mid_regex}[\s$]'
-    print(regex)
     if re.search(regex, msg_txt, re.IGNORECASE):
-        print("Sending trash")
         res = await update.message.reply_text(
             'Trash!'
         )
-        print(res)
         return None
     return None
 
